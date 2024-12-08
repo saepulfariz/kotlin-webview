b0227f4cb306e62c12991f1783b2ad375426482f
saepulfariz <saepulfariz108@gmail.com>
2024-12-08 19:29:45 +0700

    add webViewClient MainActivity

--- app/src/main/java/com/saepulfariz/kotlinwebview/MainActivity.kt
+++ app/src/main/java/com/saepulfariz/kotlinwebview/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.saepulfariz.kotlinwebview
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        webView = findViewById(R.id.webView)
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        webView.loadUrl("https://github.com/saepulfariz")
+
+        // this will enable the javascript settings, it can also allow xss vulnerabilities
+        webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+
     }
 }
